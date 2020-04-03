d7767e736a218118934432914a064e13b87955cf
Tom Butterwith <thomas.butterwith@skyscanner.net>
2020-04-03 09:41:29 +0100

    base purple theme

---
+++ src/themeVariables/purple.js
@@ -0,0 +1,31 @@
+const THEME = {
+  name: "Purple",
+  vars: {
+    type: "dark",
+    background: "#180418",
+    editor_background: "#240625",
+    button_background: "#0e020e",
+    base_text: "#adcadd",
+    selection_background: "#490b4a",
+    cursor: "#f3f2f4",
+    comment: "#5a7690",
+    function_arg: "#fd971f",
+    strings: "#ffdb89",
+    constants: "#b094de",
+    keywords: "#ea6442",
+    class_names: "#1abc9c",
+    error_state: "#8cc8ec",
+    data_types: "#e74c3c",
+    decorators: "#e6db74",
+    find_in_file_line_numbers: "#b094de", // is this needed?
+    markdown_bold: "#66d9ef",
+    other_variable: "#2980b9",
+    diff_header: "#75715e",
+    token_info: "#6796e6",
+    token_debug: "#b094de",
+    token_warn: "#cd9731",
+    token_error: "#f44747",
+  }
+};
+
+module.exports = THEME;
